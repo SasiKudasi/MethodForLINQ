592c5f176a626db9ae25c0e2e963910631fb7a7d
SasiKudasi <sasha.mittal@yandex.ru>
2024-04-03 20:16:26 +0300

    В рамках домашнего задания реализован метод-расширение для коллекции IEnumerable Top, который возвращает заданный процент от всех элементов коллекции

--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
-﻿namespace MethodForLINQ
+﻿using System.Linq;
+
+namespace MethodForLINQ
 {
-    internal class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var list = new List<int>() { 1, 2, 3, 45, 5, 7, 9, 3, 9 };
+            var list1 = list.Top(30);
+            foreach (int i in list1)
+            {
+                Console.Write($"{i} ");
+            }
+
+        }
+
+        static IEnumerable<T> Top<T>(this IEnumerable<T> source, int count)
+        {
+            if (count < 0 || count > 100)
+                throw new ArgumentException("Недопустимое значение");
+
+            var needElementsCount = source.Count() * (count / 100.0);
+            if (needElementsCount != (int)needElementsCount)
+            {
+                needElementsCount++;
+            }
+            var result = source.OrderByDescending(x => x)
+                .Take((int)needElementsCount);
+            return result;
+        }
+        static IEnumerable<T> Top<T>(this IEnumerable<T> source, int count, T field)
+        {
+            return source;
         }
     }
 }
